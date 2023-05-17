745361324e52a55bd5e7fb610b9eedfed99e9c06
Mauricio Rodriguez <titor97@gmail.com>
2023-05-16 21:42:01 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card">
+        <img className="qrImage" src="../image-qr-code.png" />
+        <p className="title">Improve your front-end skills by building projects</p>
+        <p className="paragraph">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
